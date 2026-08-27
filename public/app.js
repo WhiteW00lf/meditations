d02bf922a84cc6a366eede3f96decaa033094b0a
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-08-27 17:04:03 +0530

    added login client code

--- public/app.js
+++ public/app.js
@@ -1,24 +1,62 @@
 
-let form = document.getElementById('form');
-form.addEventListener("submit",async (e) => {
-    e.preventDefault();
-    const username  = form.username.value;
-    const password = form.password.value;
 
-    const res = await axios.post("http://localhost:8000/users",{
+let forms = document.getElementById('signupform');
+forms?.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = forms.username.value;
+    const password = forms.password.value;
+
+    const res = await axios.post("http://localhost:8000/users", {
         username,
-       password
-    }).then((res) => { 
+        password
+    }).then((res) => {
         console.log(res.data)
-        window.location.href="/login"
-    
-    
-})
-    .catch((error) => console.error(error))
+        window.location.href = "/login"
+
+
+    })
+        .catch((error) => console.error(error))
 
     // console.log(username);
     // console.log(password);
-    
-    
+
+
 
 });
+
+
+
+//login form  
+
+
+let loginform = document.getElementById("loginform");
+
+loginform?.addEventListener("submit", async (e) => {
+
+    try {
+    e.preventDefault();
+    const username = loginform.username.value;
+    const password = loginform.password.value;
+
+
+
+    const res = await axios.post("http://localhost:8000/loginusers", {
+        username,
+        password
+    });
+    localStorage.setItem("tokenfromsurya", res.data.token);
+    window.location.href ="/dashboard";
+        //window.location.href = "/dashboard";
+    
+
+
+}catch(error){
+    if(error.status === 404){
+        window.location.href = "signup";
+    }else{
+
+    console.error(error.response);
+    }
+}
+});
+
