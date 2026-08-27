72ae8c9877f4615c47603623bc3b0b400bcfd73e
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-08-27 17:03:29 +0530

    added login code

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import type { Request, Response } from "express";
-import path from "path";
+import path, { resolve } from "path";
 import { prisma } from "./prismaclient";
 import bcrypt from "bcrypt"
 import type { User } from "./generated/prisma/client";
+import jwt from 'jsonwebtoken';
+import { env } from "prisma/config";
 
 
 const app = express();
@@ -14,13 +16,19 @@ app.get("/", (req: Request, res: Response) => {
     res.status(200).send("Hello from meditation");
 });
 
+app.get("/dashboard", (req: Request, res: Response) => {
+    res.send("Dashboard");
+
+
+});
+
 app.get("/signup", (req: Request, res: Response) => {
     res.status(200).sendFile(path.join(__dirname, "public/signup.html"));
 
 });
 
 app.get("/login", (req: Request, res: Response) => {
-    res.status(200).send("Login Page");
+    res.status(200).sendFile(path.join(__dirname, "public/login.html"));
 
 });
 
@@ -44,7 +52,7 @@ app.post("/users", async (req: Request, res: Response) => {
 
         if (userExists) {
 
-            res.json({ "message": "User already exists" });
+            return res.json({ "message": "User already exists" });
 
         }
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -67,6 +75,58 @@ app.post("/users", async (req: Request, res: Response) => {
 
 
 });
+
+app.post("/loginusers", async (req: Request, res: Response) => {
+
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+        //    console.log(username);
+        //    console.log(password);
+
+        const userExists = await prisma.user.findFirst({
+            where: {
+                name: {
+                    equals: username,
+                    mode: 'insensitive'
+                }
+
+            }
+
+
+        })
+
+        console.log(userExists);
+
+        if (userExists === null) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        let unhashPassword = await bcrypt.compare(password, userExists.password);
+        if (unhashPassword) {
+
+            let jwt_secret = process.env.SECRET as string;
+            let token = jwt.sign({ username: userExists.id }, jwt_secret, { expiresIn: "1 hr" });
+            return res.status(200).json({ "message": "Login successful", "token": token });
+        } else {
+            return res.status(403).json({ "message": "Incorrect username or password" });
+
+        }
+
+
+
+
+    } catch (error: any) {
+        console.log(error.code);
+
+    }
+
+
+});
+
+
+
+
 
 
 app.listen(8000, () => {
